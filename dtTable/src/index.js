6ed18867c4196077b73fd575d2ad6fe55f90c58b
Nikita Kalitin <nikita.s.kalitin@gmail.com>
2019-11-29 13:02:49 +0300

    add

--- dtTable/src/index.js
+++ dtTable/src/index.js
@@ -1,9 +1,6 @@
-<<<<<<< HEAD
-sexport default (a, b) => a + b;s
-=======
 import { runApp } from './dtTable';
 /* eslint-disable */
 let obj = document.getElementById('app');
 runApp(obj);
 /* eslint-enable */
->>>>>>> create_boilerplate_for_js_developing
+
