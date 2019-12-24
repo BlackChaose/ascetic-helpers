c5caf1b16f3b62c24792eca95968aa30e23c35ac
Nikita Kalitin <nikita.s.kalitin@gmail.com>
2019-12-24 14:54:50 +0300

    css(mobileInput.css): dropdown ul

--- mobileInput/engine/script_sanitize_mobile_and_save_old_mobile.php
+++ mobileInput/engine/script_sanitize_mobile_and_save_old_mobile.php
@@ -15,7 +15,7 @@ ini_set('display_errors', 1);
 error_reporting(E_ERROR);
 
 require_once 'env.php';
-
+$time_start = microtime(true);
 if(!empty($_GET) && isset($_GET['start']) && isset($_GET['end'])) {
     $pdoObj = new DbUtility();
     $pdo = $pdoObj->getPDO();
@@ -36,9 +36,11 @@ if(!empty($_GET) && isset($_GET['start']) && isset($_GET['end'])) {
             'Mobile_old' => $el['Mobile']
         ];
     }, $list);
-    echo "<pre>";
-    print_r($newList);
-    echo "</pre>";
+
+//    echo "<pre>";
+//    print_r($newList);
+//    echo "</pre>";
+
     // write Mobile_sanitized
     try {
         $pdo = $pdoObj->getPDO();
@@ -51,7 +53,7 @@ if(!empty($_GET) && isset($_GET['start']) && isset($_GET['end'])) {
         }
 
         echo "<pre>";
-        print_r("Ok!");
+        print_r("write Mobile_sanitized is Ok!");
         echo "</pre>";
     } catch (\Exception $e) {
         echo "<pre>";
@@ -65,13 +67,15 @@ if(!empty($_GET) && isset($_GET['start']) && isset($_GET['end'])) {
         $queryUpdate = "UPDATE reestr.Experts SET Mobile_old = :mobOld where IdExpert = :id_expert";
         $stmt = $pdo->prepare($queryUpdate);
         foreach ($newList as $val) {
-            $stmt->bindParam(':mobOld', $val['Mobile_Old'], PDO::PARAM_STR);
-            $stmt->bindParam(':id_expert', $val['IdExpert'], PDO::PARAM_STR);
-            $stmt->execute();
+            if($val['Mobile_old'] !== $val['Mobile']) {
+                $stmt->bindParam(':mobOld', $val['Mobile_old'], PDO::PARAM_STR);
+                $stmt->bindParam(':id_expert', $val['IdExpert'], PDO::PARAM_STR);
+                $stmt->execute();
+            }
         }
 
         echo "<pre>";
-        print_r("Ok!");
+        print_r("write Mobile_old is Ok!");
         echo "</pre>";
 
     } catch (\Exception $e) {
@@ -79,7 +83,34 @@ if(!empty($_GET) && isset($_GET['start']) && isset($_GET['end'])) {
         print_r(["error!" => $e->getMessage()]);
         echo "</pre>";
     }
+
+    //write Mobile
+    try {
+        $pdo = $pdoObj->getPDO();
+        $queryUpdate = "UPDATE reestr.Experts SET Mobile = :mob where IdExpert = :id_expert";
+        $stmt = $pdo->prepare($queryUpdate);
+        foreach ($newList as $val) {
+            $stmt->bindParam(':mob', $val['Mobile_sanitized'], PDO::PARAM_STR);
+            $stmt->bindParam(':id_expert', $val['IdExpert'], PDO::PARAM_STR);
+            $stmt->execute();
+        }
+
+        echo "<pre>";
+        print_r("write Mobile_sanitized to Mobile is Ok!");
+        echo "</pre>";
+
+    } catch (\Exception $e) {
+        echo "<pre>";
+        print_r(["error!" => $e->getMessage()]);
+        echo "</pre>";
+    }
+
 }else {
     echo 'no start/end info';
 }
+$time_end = microtime(true);
+
+$execution_time = ($time_end - $time_start)/60;
+
+echo "total execution time: ".$execution_time;
 
