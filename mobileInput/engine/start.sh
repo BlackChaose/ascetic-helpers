422c84268d809b119306f9971ee120fd9d32a440
Nikita Kalitin <nikita.s.kalitin@gmail.com>
2019-12-24 14:57:36 +0300

    add(start.sh): run script_sanitize_mobile_save_old_mobile.php for update DB

--- mobileInput/engine/start.sh
+++ mobileInput/engine/start.sh
@@ -1,1 +1,11 @@
 #!/usr/bin/env bash
+
+#curl -X GET 'http://localhost:5000/script_sanitize_mobile_and_save_old_mobile.php?start=0&end=1000'
+echo "!!!!!!"
+for END in {1000..8000..1000}
+do
+   let START=$END-1000
+   ADDRESS="http://localhost:5000/script_sanitize_mobile_and_save_old_mobile.php?start=$START&end=$END"
+   curl -I -X GET $ADDRESS
+done
+echo "!END"
