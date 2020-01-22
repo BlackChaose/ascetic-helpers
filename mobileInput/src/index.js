dace5512354935c982f7da96602cf876775fd598
Nikita Kalitin <nikita.s.kalitin@gmail.com>
2020-01-22 09:42:02 +0300

    fix(index.js): add condition for checking current path

--- mobileInput/src/index.js
+++ mobileInput/src/index.js
@@ -3,14 +3,16 @@ import renderMobileInput from './mobileInputComponent';
 
 /* eslint-disable */
 document.addEventListener('DOMContentLoaded', () => {
-  const config = {
-    "domObject": document.getElementById('appInput'),
-    "url": 'ajax_mobile_codes.php',
-    "defaultMobile": document.getElementById('appInput').dataset.mobileVal,
-    "defaultCountryCode": document.getElementById('appInput').dataset.mobileCode,
-    "hiddenInputName":document.getElementById('appInput').dataset.nameInput,
-    "borderStyle":document.getElementById('appInput').dataset.borderStyle,
-  };
-  renderMobileInput(config);
+  if(window.location.pathname === '/experty/expert_change_dom.php') {
+    const config = {
+      "domObject": document.getElementById('appInput'),
+      "url": 'ajax_mobile_codes.php',
+      "defaultMobile": document.getElementById('appInput').dataset.mobileVal,
+      "defaultCountryCode": document.getElementById('appInput').dataset.mobileCode,
+      "hiddenInputName": document.getElementById('appInput').dataset.nameInput,
+      "borderStyle": document.getElementById('appInput').dataset.borderStyle,
+    };
+    renderMobileInput(config);
+  }
 });
 /* eslint-enable */
