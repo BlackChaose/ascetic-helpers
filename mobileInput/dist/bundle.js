fbfbfa3a431d6dee4f4261674381ac61f32df64d
Nikita Kalitin <nikita.s.kalitin@gmail.com>
2020-01-22 09:45:00 +0300

    asm(mobileInput): assemble after fix index.js

--- mobileInput/dist/bundle.js
+++ mobileInput/dist/bundle.js
@@ -9,15 +9,17 @@ function _interopRequireDefault(obj) { return obj && obj.__esModule ? obj : { de
 
 /* eslint-disable */
 document.addEventListener('DOMContentLoaded', function () {
-  var config = {
-    "domObject": document.getElementById('appInput'),
-    "url": 'ajax_mobile_codes.php',
-    "defaultMobile": document.getElementById('appInput').dataset.mobileVal,
-    "defaultCountryCode": document.getElementById('appInput').dataset.mobileCode,
-    "hiddenInputName": document.getElementById('appInput').dataset.nameInput,
-    "borderStyle": document.getElementById('appInput').dataset.borderStyle
-  };
-  (0, _mobileInputComponent.default)(config);
+  if (window.location.pathname === '/experty/expert_change_dom.php') {
+    var config = {
+      "domObject": document.getElementById('appInput'),
+      "url": 'ajax_mobile_codes.php',
+      "defaultMobile": document.getElementById('appInput').dataset.mobileVal,
+      "defaultCountryCode": document.getElementById('appInput').dataset.mobileCode,
+      "hiddenInputName": document.getElementById('appInput').dataset.nameInput,
+      "borderStyle": document.getElementById('appInput').dataset.borderStyle
+    };
+    (0, _mobileInputComponent.default)(config);
+  }
 });
 /* eslint-enable */
 
